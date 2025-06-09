69e0d4d98f50241f8cae698e3646a877b0f25653
Alvaro <0001065399@senaimgaluno.com.br>
2025-06-09 11:11:40 -0300

    Adicionar arquivos de projeto.

---
+++ ChatServer/ChatHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+public class ChatHub : Hub
+{
+    public async Task EnviarMensagem(string usuario, string mensagem)
+    {
+        await Clients.All.SendAsync("ReceberMensagem", usuario, mensagem);
+    }
+}
