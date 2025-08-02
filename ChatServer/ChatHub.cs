09998dd24dee76b8f14fe3997e4e189090e46d6b
Alvaro Gabriel <Silas@PC>
2025-08-02 13:04:02 -0300

    Pedido de Amizade Feito Sem Imagem do usuario

--- ChatServer/ChatHub.cs
+++ ChatServer/ChatHub.cs
@@ -9,18 +9,18 @@ namespace ChatServer
 {
     public class ChatHub : Hub
     {
-        private static ConcurrentDictionary<string, string> _usuarios = new ConcurrentDictionary<string, string>();
+        private static ConcurrentDictionary<int, string> _usuarios = new ConcurrentDictionary<int, string>();
 
-        public async Task RegistrarUsuario(string idUsuario)
+        public async Task RegistrarUsuario(int idUsuario)
         {
+            Console.WriteLine("Tentando registrar ID: {0}", idUsuario);
             _usuarios[idUsuario] = Context.ConnectionId;
 
             await Clients.All.SendAsync("UsuarioConectado", idUsuario);
             Console.WriteLine($"Usuário registrado: {idUsuario} com ConnectionId: {Context.ConnectionId}");
         }
 
-
-        public async Task EnviarMensagemPrivada(string paraIdUsuario, string deIdUsuario, string mensagem)
+        public async Task EnviarMensagemPrivada(int paraIdUsuario, int deIdUsuario, string mensagem)
         {
             if (_usuarios.TryGetValue(paraIdUsuario, out string connIdDestino))
             {
@@ -33,24 +33,24 @@ namespace ChatServer
             }
         }
 
-
-        public async Task EnviarSolicitacaoAmizade(string idDestino, string meuId)
+        public async Task EnviarSolicitacaoAmizade(int idDestino, int meuId)
         {
+            Console.WriteLine($"Enviando solicitação de amizade de {meuId} para {idDestino}");
             var dao = new PedidoAmizadeDAO();
-            dao.SalvarPedido(new PedidoAmizade { DeUsuario = idDestino, ParaUsuario = meuId });
+            dao.SalvarPedido(new PedidoAmizade { DeUsuario = meuId, ParaUsuario = idDestino });
 
             if (_usuarios.TryGetValue(idDestino, out string connIdDestino))
             {
+                Console.WriteLine($"Usuário destino conectado, enviando via SignalR");
                 await Clients.Client(connIdDestino).SendAsync("ReceberSolicitacaoAmizade", meuId);
                 await Clients.Caller.SendAsync("SolicitacaoEnviada", idDestino);
             }
             else
             {
+                Console.WriteLine($"Usuário destino OFFLINE, enviando resposta offline");
                 await Clients.Caller.SendAsync("SolicitacaoEnviadaOffline", idDestino);
             }
         }
-
-
 
         public override async Task OnConnectedAsync()
         {
